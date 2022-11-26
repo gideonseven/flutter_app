4fee2660a83b74bf76bd99ac2bdbbe3e4975a5c0
Gideon <gideon@cicil.co.id>
2022-11-26 14:50:28 +0700

    add first layer

--- lib/main.dart
+++ lib/main.dart
@@ -1,64 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/detail_screen.dart';
 
+void main() => runApp(const MyApp());
 
-void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const Scaffold(
-        body: Center(
-          child: BiggerText(text:"Hello world!"), // Ubah widget Heading ke PerubahanText
-        ),
-      ),
-    );
-  }
-}
-
-class Heading extends StatelessWidget {
-  final String text;
-
-  const Heading({Key? key, required this.text}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Text(
-      text,
-      style: const TextStyle(
-        fontSize: 24.0,
-        fontWeight: FontWeight.bold,
-      ),
-    );
-  }
-}
-
-class BiggerText extends StatefulWidget {
-  final String text;
-  const BiggerText({Key? key, required this.text}) : super(key: key);
-  @override
-  _BiggerTextState createState() => _BiggerTextState();
-}
-class _BiggerTextState extends State<BiggerText> {
-  double _textSize = 16.0;
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        Text(widget.text, style: TextStyle(fontSize: _textSize)),
-        ElevatedButton(
-          child: const Text("Perbesar"),
-          onPressed: () {
-            setState(() {
-              _textSize = 32.0;
-            });
-          },
-        )
-      ],
+      title: 'Wisata Bandung',
+      theme: ThemeData(),
+      home: const DetailScreen(),
     );
   }
 }
